ddeff56eada22aa3d9da931e6bb8a5cd9d5c0d55
Sander Greni <sander@datek.no>
2025-03-26 14:58:58 +0100

    Move definitions to the cpp file where they belong

--- thread_pool.h
+++ thread_pool.h
@@ -17,65 +17,12 @@ namespace xo::thread_pool
 		using tasks_t = std::queue<task_t>;
 
 	public:
-		thread_pool_t(size_t thread_count)
-		{
-			std::ranges::for_each(std::views::iota(0, static_cast<int>(thread_count)), [this](auto)
-				{
-					workers.emplace_back([this](std::stop_token stoken)
-						{
-							while (!stoken.stop_requested())
-							{
-								task_t task;
-								{
-									std::unique_lock<std::mutex> lock(queue_mutex);
-									bool const there_is_work_to_do = condition.wait(lock, stoken, [this]
-										{
-											return !tasks.empty();
-										});
-
-									if (stoken.stop_requested())
-									{
-										break;
-									}
-									else if (there_is_work_to_do)
-									{
-										task = std::move(tasks.front());
-										tasks.pop();
-									}
-								}
-
-								if (task)
-								{
-									if (stoken.stop_requested())
-									{
-										break;
-									}
-
-									task(stoken);
-								}
-							}
-						});
-				});
-		}
-
+		thread_pool_t(size_t thread_count);
 		thread_pool_t(thread_pool_t const&) = delete;
 		thread_pool_t(thread_pool_t&&) = delete;
 		thread_pool_t& operator=(thread_pool_t const&) = delete;
 		thread_pool_t& operator=(thread_pool_t&&) = delete;
-
-		~thread_pool_t()
-		{
-			{
-				std::unique_lock<std::mutex> lock(queue_mutex);
-				tasks_t tmp;
-				tmp.swap(tasks); // In the absense of clear()
-			}
-
-			std::ranges::for_each(workers, [this](std::jthread& worker)
-				{
-					worker.request_stop();
-				});
-		}
+		~thread_pool_t();
 
 		template<typename F, typename... Args>
 		auto enqueue(F&& f, Args... args)
